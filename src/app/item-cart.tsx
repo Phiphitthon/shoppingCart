7b0b22274e830a6a1c125f6e2c5a756efd38a40b
HoopShop.TH <119285445+Phiphitthon@users.noreply.github.com>
2024-09-02 19:45:40 +0700

    first commit

---
+++ src/app/item-cart.tsx
@@ -0,0 +1,82 @@
+import {
+    IconButton,
+    Stack,
+    Typography,
+    Grid,
+    Card,
+    CardContent,
+    CardMedia,
+  } from "@mui/material";
+  import AddIcon from "@mui/icons-material/Add";
+  import RemoveIcon from "@mui/icons-material/Remove";
+  import React from "react";
+  
+  export default function ItemCart({
+    itemname,
+    itemPrice,
+    image,
+    handleIncrement,
+    handleDecrement,
+  }: {
+    itemname: string;
+    itemPrice: number;
+    image: string;
+    handleIncrement: (price: number) => void;
+    handleDecrement: (price: number) => void;
+  }) {
+    const [count, setCount] = React.useState(0);
+    const [totalPrice, setTotalPrice] = React.useState(0);
+  
+    const handleAddItemClick = () => {
+      const newCount = count + 1;
+      setCount(newCount);
+      const newTotalPrice = newCount * itemPrice;
+      setTotalPrice(newTotalPrice);
+      handleIncrement(itemPrice);
+    };
+  
+    const handleDeleteItemClick = () => {
+      if (count === 0) {
+        return;
+      }
+      const newCount = count - 1;
+      setCount(newCount);
+      const newTotalPrice = newCount * itemPrice;
+      setTotalPrice(newTotalPrice);
+      handleDecrement(itemPrice);
+    };
+  
+    return (
+      <Grid item xs={12} sm={6} md={4}>
+        <Card>
+          <CardMedia
+            component="img"
+            height="140"
+            image={image}
+            alt={itemname}
+            sx={{ 
+              objectFit: "contain",  // ทำให้ภาพแสดงทั้งหมดภายในกรอบ
+              objectPosition: "center", // ทำให้ภาพอยู่ตรงกลาง
+            }}
+          />
+          <CardContent >
+            <Typography variant="h6"  gutterBottom >
+              {itemname}
+            </Typography>
+            <Stack direction="row" spacing={2} alignItems="center">
+              <IconButton onClick={handleDeleteItemClick}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography variant="h6">{count}</Typography>
+              <IconButton onClick={handleAddItemClick}>
+                <AddIcon />
+              </IconButton>
+            </Stack>
+            <Typography variant="h6" textAlign="center" mt={2}>
+              {totalPrice.toLocaleString()} baht
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { Stack, Typography, Container, Grid } from "@mui/material";
+import React from "react";
+import ItemCart from "./item-cart";
 
 export default function Home() {
+  const [totalItems, setTotalItems] = React.useState(0);
+  const [grandTotal, setGrandTotal] = React.useState(0);
+
+  const handleIncrement = (price: number) => {
+    setTotalItems(totalItems + 1);
+    setGrandTotal(grandTotal + price);
+  };
+
+  const handleDecrement = (price: number) => {
+    if (totalItems > 0) {
+      setTotalItems(totalItems - 1);
+      setGrandTotal(grandTotal - price);
+    }
+  };
+
+  const myItems = [
+    { itemname: "iPhone15", price: 32900, image: "http://store.storeimages.cdn-apple.com/8https756/as-images.apple.com/is/iphone15-digitalmat-gallery-1-202309?wid=728&hei=666&fmt=png-alpha&.v=1693346853713" },
+    { itemname: "iPhone15 Plus", price: 37900, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone15-digitalmat-gallery-1-202309?wid=728&hei=666&fmt=png-alpha&.v=1693346853713"},
+    { itemname: "iPhone15 Pro", price: 41900, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone15pro-digitalmat-gallery-1-202309?wid=728&hei=666&fmt=png-alpha&.v=1693346851364" },
+    { itemname: "iPhone15 Pro Max", price: 48900, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone15pro-digitalmat-gallery-1-202309?wid=728&hei=666&fmt=png-alpha&.v=1693346851364" },
+    { itemname: "iPad", price: 13100, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-digitalmat-gallery-1-202210?wid=728&hei=666&fmt=png-alpha&.v=1664392871297" },
+    { itemname: "iPad mini", price: 17900, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-mini-digitalmat-gallery-1-202111?wid=730&hei=666&fmt=png-alpha&.v=1635183174000" },
+    { itemname: "iPad Air", price: 21900, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipadair11-digitalmat-gallery-1-202404?wid=728&hei=666&fmt=jpeg&qlt=90&.v=1713308648429" },
+    { itemname: "iPad Pro", price: 35900, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipadpro11-digitalmat-gallery-1-202404?wid=728&hei=666&fmt=png-alpha&.v=1713308651643" },
+    { itemname: "iMac", price: 48000 , image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/imac24-digitalmat-gallery-1-202310?wid=728&hei=666&fmt=png-alpha&.v=1697559004117"},
+    { itemname: "MacBook Air", price: 31400, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mba13-m2-digitalmat-gallery-1-202402?wid=728&hei=666&fmt=png-alpha&.v=1707263976493" },
+    { itemname: "MacBook Pro", price: 56200 , image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mbp-14-digitalmat-gallery-1-202310?wid=728&hei=666&fmt=png-alpha&.v=1696971522628"},
+    { itemname: "Mac mini", price: 20900, image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mac-mini-digitalmat-gallery-1-202301?wid=728&hei=666&fmt=png-alpha&.v=1670038163709"},
+    { itemname: "Mac Studio", price: 74900 , image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mac-studio-digitalmat-gallery-1-202306?wid=728&hei=666&fmt=png-alpha&.v=1683914326094"},
+  ];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Container>
+      <Typography variant="h3" gutterBottom>
+        Shopping Cart
+      </Typography>
+      <Grid container spacing={4}>
+        {myItems.map((item) => (
+          <ItemCart
+            key={item.itemname}
+            itemname={item.itemname}
+            itemPrice={item.price}
+            image={item.image}
+            handleIncrement={handleIncrement}
+            handleDecrement={handleDecrement}
+          />
+        ))}
+      </Grid>
+      <Stack direction="row" spacing={2} justifyContent="space-between" mt={2}>
+        <Typography variant="h5">total:</Typography>
+        <Typography variant="h5">
+          {grandTotal.toLocaleString()} baht
+        </Typography>
+      </Stack>
+    </Container>
   );
 }
